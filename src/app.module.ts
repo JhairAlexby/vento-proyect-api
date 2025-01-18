36e80bbcb683674a44b0393e8e468c73fb521ca2
JhairAlexby <jhair.alexby1@gmail.com>
2025-01-18 00:40:32 -0600

    Add Mongoose and Config modules, update User DTO for partial updates

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
-  imports: [UserModule],
+  imports: [
+
+    ConfigModule.forRoot(),
+    
+    UserModule,
+
+    MongooseModule.forRoot(process.env.MONGODB),
+
+
+
+  
+  ],
   
 })
-export class AppModule {}
+export class AppModule {
+  constructor(){
+    console.log(process.env)
+  }
+}
